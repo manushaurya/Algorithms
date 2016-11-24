4ac6cc379f63a5149832cedd46a27216d4d6ebce
Manu Shaurya <shauryamanu50@gmail.com>
2016-11-24 12:35:32 +0530

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,75 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void heapsort(int heap[],int n)
+void reheapifydown(int heap[],int start, int finish)
 {
-	int i,temp;
-	heapify(heap,n);
-	for(i=n;i>=1;i--)
-	{
-		temp=heap[1];
-		heap[1]=heap[i];
-		heap[i]=temp;
-		reheapifydown(heap,1,i-1);
-	}
-}
-void heapify(int heap[],int n)
-{
-	int i;
-	int index=n/2;
-	for(i=index;i>=1;i--)
-	{
-		reheapifydown(heap,i,n);
-	}
+    int max,temp,index;
+    int lchild=2*start;
+    int rchild=lchild+1;
+    if(lchild<=finish)
+    {
+        max=heap[lchild];
+        index=lchild;
+        if(rchild<=finish)
+        {
+            if(heap[rchild]>max)
+            {
+                max=heap[rchild];
+                index=rchild;
+            }
+        }
+        if(heap[start]<heap[index])
+        {
+            temp=heap[start];
+            heap[start]=heap[index];
+            heap[index]=temp;
+            reheapifydown(heap,index,finish);
+        }
+    }
 }
 
-void reheapifydown(int heap[],int start,int finish)
+void heapify(int heap[],int n)
 {
-	int max,temp,index;
-	int lchild=2*start;
-	int rchild=lchild+1;
-	if(lchild<=finish)
-	{
-		max=heap[lchild];
-		index=lchild;
-		if(rchild<=finish)
-		{
-			if(heap[rchild]>max)
-			{
-				max=heap[rchild];
-				index=rchild;
-			}
-		}
-		if(heap[start]<heap[index])
-		{
-			temp=heap[start];
-			heap[start]=heap[index];
-			heap[index]=temp;
-			reheapifydown(heap,index,finish);
-		}
-	}
+    int i;
+    int index=n/2;
+    for(i=index;i>=1;i--)
+    {
+        reheapifydown(heap,i,n);
+    }
+}
+
+void heapsort(int heap[],int n)
+{
+    int i,temp;
+    heapify(heap,n);
+    for(i=n;i>=1;i--)
+    {
+        temp=heap[1];
+        heap[1]=heap[i];
+        heap[i]=temp;
+        reheapifydown(heap,1,i-1);
+    }
 }
 
 int main()
 {
-	int n,i;
-	printf("Enter the no. of elements to be entered:\n");
-	scanf("%d",&n);
-	int a[n];
-	for(i=1;i<=n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	printf("Unsorted array is:\n");
-	for(i=1;i<=n;i++)
-	{
-		printf("%d ",a[i]);
-	}
-	printf("\n\nSorted array is :\n");
-	heapsort(a,n);
-	for(i=1;i<=n;i++)
-	{
-		printf("%d ",a[i]);
-	}
-	return 0;
+    int n,i;
+    printf("Enter the no. of elements to be entered: ");
+    scanf("%d",&n);
+    int a[n];
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Unsorted array is:\n");
+    for(i=1;i<=n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n\nSorted array is:\n");
+    heapsort(a,n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
 }
